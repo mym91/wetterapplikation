6d7a489da43d5360f9a93018439802d0619069ab
Myriam Koch <myrikoch@hotmail.com>
2014-05-16 11:29:08 +0200

    edit script.js: remove dummy code; add begin of ajax request for forecast api

--- script.js
+++ script.js
@@ -1,37 +1,5 @@
 // JavaScript Document
-function chkFormular () {
-  if (document.Formular.User.value == "") {
-    alert("Bitte Ihren Namen eingeben!");
-    document.Formular.User.focus();
-    return false;
-  }
-  if (document.Formular.Ort.value == "") {
-    alert("Bitte Ihren Wohnort eingeben!");
-    document.Formular.Ort.focus();
-    return false;
-  }
-  if (document.Formular.Mail.value == "") {
-    alert("Bitte Ihre E-Mail-Adresse eingeben!");
-    document.Formular.Mail.focus();
-    return false;
-  }
-  if (document.Formular.Mail.value.indexOf("@") == -1) {
-    alert("Keine E-Mail-Adresse!");
-    document.Formular.Mail.focus();
-    return false;
-  }
-  if (document.Formular.Alter.value == "") {
-    alert("Bitte Ihr Alter eingeben!");
-    document.Formular.Alter.focus();
-    return false;
-  }
-  var chkZ = 1;
-  for (i = 0; i < document.Formular.Alter.value.length; ++i)
-    if (document.Formular.Alter.value.charAt(i) < "0" ||
-        document.Formular.Alter.value.charAt(i) > "9")
-      chkZ = -1;
-  if (chkZ == -1) {
-    alert("Altersangabe keine Zahl!");
-    document.Formular.Alter.focus();
-    return false;
-  }
+var service = "https://api.forecast.io/forecast/9f6923aa30e557f8db747d39ae4fa19d/47.306613,%209.318580,0?units=si";
+$.ajax({
+	url: service
+});
